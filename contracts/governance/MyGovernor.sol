ad7c88d9b62d0fc1f45cb9931b2f1ce11c9280bc
TheStarBoys <shengzang19980@163.com>
2022-03-02 23:24:51 +0800

    Add governance contracts

---
+++ contracts/governance/MyGovernor.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+
+contract MyGovernor is Governor, GovernorVotes, GovernorVotesQuorumFraction, GovernorCountingSimple {
+    constructor(IVotes _token)
+        Governor("MyGovernor")
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4)
+    {}
+
+    function votingDelay() public pure override returns (uint256) {
+        // return 6575; // 1 day
+        return 300; // 5 mins
+    }
+
+    function votingPeriod() public pure override returns (uint256) {
+        // return 46027; // 1 week
+        return 300; // 5 mins
+    }
+
+    function proposalThreshold() public pure override returns (uint256) {
+        return 0;
+    }
+
+    // The functions below are overrides required by Solidity.
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function getVotes(address account, uint256 blockNumber)
+        public
+        view
+        override(IGovernor, GovernorVotes)
+        returns (uint256)
+    {
+        return super.getVotes(account, blockNumber);
+    }
+}
